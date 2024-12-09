a91a13ec79888eac7e446ad2f5ccc4212e33806d
suasrp <suasrp@gmail.com>
2024-12-09 11:00:45 -0600

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -16,7 +16,7 @@ def get_current_page_name():
 
 def make_sidebar():
     with st.sidebar:
-        st.title("💎 Diamond Corp")
+        st.title("⚡ Cheatham Speller")
         st.write("")
         st.write("")
 
