8271471933a4f06082330469a6ad4690eebcd60e
suasrp <suasrp@gmail.com>
2024-12-09 12:14:57 -0600

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -21,7 +21,7 @@ def make_sidebar():
         st.write("")
 
         if st.session_state.get("logged_in", False):
-            st.page_link("pages/page1.py", label="Secret Company Stuff", icon="🔒")
+            st.page_link("pages/page1.py", label="6th Grade - List of Words", icon="🕵️ 🔒")
             st.page_link("pages/page2.py", label="More Secret Stuff", icon="🕵️")
 
             st.write("")
