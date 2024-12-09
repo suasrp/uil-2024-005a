3c6b36a9e52281391da63284973c02c62a92cdeb
suasrp <suasrp@gmail.com>
2024-12-09 12:29:05 -0600

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,20 +1,3 @@
-#from navigation import make_sidebar
-#import streamlit as st
-
-#make_sidebar()
-
-#st.write(
-    """
-# 👍 6th Grade - Spelling Game
-
-#This is a secret page that only logged-in users can see.
-
-#Super duper secret.
-
-#Shh....
-
-#"""
-#)
 
 import streamlit as st
 from gtts import gTTS
