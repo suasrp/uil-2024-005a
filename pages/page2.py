01dcaf5f36b3e80d61ebff2c322ed5814ecf73b0
suasrp <suasrp@gmail.com>
2024-12-09 13:08:30 -0600

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,21 +1,18 @@
 from navigation import make_sidebar
 import streamlit as st
 
-
 make_sidebar()
 
 st.write(
     """
 # 👍 6th Grade - Spelling Game
 
-This is a secret page that only logged-in users can see.
-
-Super duper secret.
-
-Shh....
+Let's have some spelling fun!
 
 """
+
 )
+
 
 # Updated list of words
 words = [
