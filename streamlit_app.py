402fcb2c5fc8b256e07057ba952d3bcf6ede7194
suasrp <suasrp@gmail.com>
2024-12-12 14:17:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,18 @@ from navigation import make_sidebar
 
 make_sidebar()
 
-st.title("Welcome to Cheatham Speller")
+st.title("Welcome to Cheatham Speller β")
 
-st.write("🔒 Please log in to continue (username `test`, password `test`).")
+st.write("🔒 Please log in to continue.")
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Log in", type="primary"):
-    if username == "test" and password == "test":
+    if username == "uiltest" and password == "cheathamtest":
         st.session_state.logged_in = True
-        st.success("Logged in successfully!")
+        st.success("✔️Logged in successfully!")
         sleep(0.5)
         st.switch_page("pages/page1.py")
     else:
-        st.error("Incorrect username or password")
+        st.error("❌Incorrect username or password")
