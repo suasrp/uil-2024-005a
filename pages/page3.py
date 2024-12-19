7e1360b265441583f619f95cc02a947ada67b3d0
suasrp <suasrp@gmail.com>
2024-12-19 14:32:39 -0600

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -3,169 +3,247 @@ import streamlit as st
 
 make_sidebar()
 
+#########################
+
+st.title(" 📝 3rd-4th Grade - List of Words")
+
+# Streamlit interface for the test
 st.write(
     """
-# 📝 3rd-4th Grade - List of Words
-
----Words starting with 'A':
-
-"abandon", "absent", "absolute", "academy", "ache",
+(A)"abandon", "absent", "absolute", "academy", "ache",
 "address", "adult", "afterglow", "against", "airliner", "alley", "allspice", "almond", "already", "alter",
 "ambition", "amusement", "ancestor", "annual", "apology", "appetite", "argue", "artist", "assign", "astronomer",
 "athlete", "autograph", "autumn", "awesome".
-    
----Words starting with 'B':
 
-"backpack", "badger", "bagel", "bait", "banana", "barefoot",
+    """
+)
+
+st.write(
+    """
+ (B)"backpack", "badger", "bagel", "bait", "banana", "barefoot",
 "bark", "barley", "barrier", "baste", "beaver", "beehive", "beige", "beverage", "beware", "biology",
 "black hole", "blade", "blockage", "blueberry", "blurt", "blush", "borrow", "bottom", "bravery", "bridesmaid",
 "bridle", "bronco", "brownie", "building", "built-in", "buttermilk".
+    
+    """
+)
 
----Words starting with 'C':
-
-"calm", "camel", "canine", "capsule", "California", 
+st.write(
+    """
+ (C)"calm", "camel", "canine", "capsule", "California", 
 "cashew", "caterpillar", "catwalk", "cautious", "cement", "century", "champion", "charming", "cheerful", "childish",
 "chuckle", "circuit", "circulate", "citizenship", "climate", "coconut", "collection", "comma", "company", "compel",
 "computer", "concern", "condone", "copy", "cousin", "creakier", "creepy", "cricket", "curtsy", "custard".
     
----Words starting with 'D':
+    """
+)
 
-"dairy", "dazzled", "deaden", "decaf", "decent", "decimal", "decline", "dentist", "dependable", "desktop",
+st.write(
+    """
+ (D)"dairy", "dazzled", "deaden", "decaf", "decent", "decimal", "decline", "dentist", "dependable", "desktop",
 "dessert", "develop", "diary", "dipper", "direction", "disappoint", "disease", "dishonest", "dismiss", "dispute",
 "distrust", "dizzy", "doggy", "dose", "downwind", "dramatic", "dribble", "drinkable", "drudge", "duet".
     
----Words starting with 'E':
+    """
+)
 
-"eagle", "earnings", "easiest", "easygoing", "echo", "economical", "edition", "educate", "eggplant", "either",
+st.write(
+    """
+ (E)"eagle", "earnings", "easiest", "easygoing", "echo", "economical", "edition", "educate", "eggplant", "either",
 "elderly", "electrify", "element", "elevator", "elude", "emotion", "empower", "enable", "ending", "energy",
 "entirely", "entrap", "entry", "error", "essay", "esteem", "ever", "example", "exchange", "exercise",
 "export", "eye-catching", "eyelash".
     
----Words starting with 'F':
+    """
+)
 
-"factual", "falcon", "famous", "farmstead", "faucet", "fearless", "ferret",
+st.write(
+    """
+ (F)"factual", "falcon", "famous", "farmstead", "faucet", "fearless", "ferret",
 "festival", "fidget", "field", "filmstrip", "fizzling", "flavor", "fleece", "foliage", "footstep", "foresight",
 "forestry", "formula", "forthright", "framed", "frank", "freaky", "frightful", "frown", "fudge", "funnel".
 "furry".
     
----Words starting with 'G':
+    """
+)
 
-"gadget", "gala", "galaxy", "garden", "garment", "garnish", "gathering", "generous", "genius",
+st.write(
+    """
+ (G)"gadget", "gala", "galaxy", "garden", "garment", "garnish", "gathering", "generous", "genius",
 "gentlefolk", "geology", "German", "germfree", "giggle", "giraffe", "glassblower", "glimpse", "gloomy", "goalpost", "goggles",
 "gosling", "grader", "granite", "graph", "gravity", "gravy", "greedy", "griddle", "grime", "grungy",
 "guardian", "guide", "guitar", "gummy", "gusto".
     
----Words starting with 'H':
+    """
+)
 
-"hairdresser", "hallway", "halt", "handful", "handsaw",
+st.write(
+    """
+ (H)"hairdresser", "hallway", "halt", "handful", "handsaw",
 "hapless", "harmonica", "harp", "harvest", "hateful", "hawk", "headfirst", "healer", "heavenly", "hefty",
 "hemline", "heritage", "hipbone", "Hispanic", "history", "hoarse", "honorable", "hotel", "hottest", "housemaid", "humid",
 "hurricane", "hurried".
     
----Words starting with 'I':
+    """
+)
 
-"igloo", "illness", "image", "immune", "impact", "imprint", "imprison", "incoming",
+st.write(
+    """
+ (I)"igloo", "illness", "image", "immune", "impact", "imprint", "imprison", "incoming",
 "index", "indigo", "infest", "inform", "inherit", "injury", "inkling", "inland", "inquest", "inscribe",
 "insertion", "insulate", "interest", "intrude", "inventive", "Irish", "iron", "irritate".
     
----Words starting with 'J':
+    """
+)
 
-"jabbed", "jacket", "jailbird",
+st.write(
+    """
+ (J)"jabbed", "jacket", "jailbird",
 "janitor", "jawbone", "jelly", "journal", "June", "junior".
+    
+    """
+)
 
----Words starting with 'K':
-
-"kennel", "keynote", "kidnap", "kindness", "kitty",
+st.write(
+    """
+ (K)"kennel", "keynote", "kidnap", "kindness", "kitty",
 "knuckle", "kook".
+    
+    """
+)
 
----Words starting with 'L':
-
-"landing", "landscape", "larva", "lately", "latitude", "lavender", "lawyer", "leader",
+st.write(
+    """
+ (L)"landing", "landscape", "larva", "lately", "latitude", "lavender", "lawyer", "leader",
 "leakage", "leap", "legally", "leotard", "liberty", "ligament", "light-footed", "likeness", "limiting", "linger",
 "lining", "listen", "liven", "lofty", "logbook", "longhorn", "loudness", "loveliness", "luminous", "lure",
 "lush".
     
----Words starting with 'M':
+    """
+)
 
-"machine", "magazine", "magical", "magnet", "majority", "mammoth", "manager", "marine", "massage",
+st.write(
+    """
+ (M)"machine", "magazine", "magical", "magnet", "majority", "mammoth", "manager", "marine", "massage",
 "massive", "mechanical", "medicine", "medley", "meow", "merge", "mermaid", "microwave", "migrant", "mineral",
 "mingled", "minimal", "misshapen", "mistletoe", "mistreat", "modest", "modify", "molasses", "muffler", "muscle",
 "mystery", "myth".
     
----Words starting with 'N':
+    """
+)
 
-"napkin", "narrate", "national", "naturalize", "nearby", "necessary", "necklace", "neglect",
+st.write(
+    """
+ (H)"napkin", "narrate", "national", "naturalize", "nearby", "necessary", "necklace", "neglect",
 "nephew", "newlywed", "nineteenth", "noel", "noggin", "nomad", "nonfiction", "nonviolent", "normal", "nosebleed",
 "noted", "notion", "nourish", "novel", "noxious", "nudging", "numeric", "nunnery", "nuzzle", "nylon".
     
----Words starting with 'O':
+    """
+)
 
-"oarsman", "obsess", "obstinate", "obtain", "oddity", "offense", "oilcloth", "okra", "once", "oodles",
+st.write(
+    """
+ (O)"oarsman", "obsess", "obstinate", "obtain", "oddity", "offense", "oilcloth", "okra", "once", "oodles",
 "oozy", "operate", "opposite", "opt", "orbit", "ordeal", "ore", "organ", "origin", "ouch",
 "ounce", "outlandish", "outside", "overcome", "overripe", "ozone".
     
----Words starting with 'P':
+    """
+)
 
-"painful", "panic", "parasite", "parish",
+st.write(
+    """
+ (P)"painful", "panic", "parasite", "parish",
 "particle", "pastry", "pasture", "patrolman", "peach", "peacock", "pecan", "pedicure", "percent", "perky",
 "perplex", "perspire", "phonics", "phrase", "physical", "pierce", "pledge", "pliable", "pointless", "positive",
 "poultice", "preacher", "precious", "predict", "priceless", "primary", "prosper", "proverb", "purebred".
     
----Words starting with 'Q':
+    """
+)
 
-"quail",
+st.write(
+    """
+ (Q)"quail",
 "quaint", "quart", "quash", "quest", "quibbling", "quilt", "quirk", "quite", "quoted".
+    
+    """
+)
 
----Words starting with 'R':
-
-"radar",
+st.write(
+    """
+ (R)"radar",
 "radio", "raggedy", "raisin", "readable", "rebate", "recess", "recollect", "refashion", "regardful", "register",
 "release", "relic", "remedy", "reporter", "reptile", "respond", "retail", "retreat", "revenge", "rhino",
 "riflery", "romance", "rooster", "rotating", "rough", "rowdy", "ruby", "rudeness", "rust".
     
----Words starting with 'S':
+    """
+)
 
-"sacred",
+st.write(
+    """
+ (S)"sacred",
 "salmon", "salute", "sarcastic", "sawdust", "scientist", "scorpion", "seamstress", "secretary", "seventh", "several",
 "shabbily", "sibling", "sketch", "slippery", "sluggish", "snoopy", "soccer", "soprano", "sourdough", "soybean",
 "spirited", "sponsor", "stadium", "stomach", "struggle", "subtract", "succeed", "suddenly", "superior", "syllable",
 "system".
     
----Words starting with 'T':
+    """
+)
 
-"tabulate", "talent", "tattered", "tattletale", "teethe", "tempting", "tennis", "term", "textbook",
+st.write(
+    """
+ (T)"tabulate", "talent", "tattered", "tattletale", "teethe", "tempting", "tennis", "term", "textbook",
 "thankfully", "theft", "thermostat", "thicken", "thievery", "throat", "thumbnail", "thunderbolt", "tickling", "timid",
 "together", "toughen", "tourism", "treasure", "tremor", "tribal", "trombone", "trouble", "tulip", "tuneful",
 "turmoil", "twitter", "typhoon".
     
----Words starting with 'U':
+    """
+)
 
-"umpire", "unable", "underhand", "underneath", "unfair", "unicorn", "uninstall",
+st.write(
+    """
+ (U)"umpire", "unable", "underhand", "underneath", "unfair", "unicorn", "uninstall",
 "unit", "universe", "unlisted", "unpleasant", "unseal", "unsure", "untangle", "upright", "upturn", "urban",
 "usable", "utterly".
 
----Words starting with 'V':
+    
+    """
+)
 
-"vacate", "valor", "valuable", "variety", "vast", "vehicle", "venom", "verdict",
+st.write(
+    """
+ (V)"vacate", "valor", "valuable", "variety", "vast", "vehicle", "venom", "verdict",
 "vibrant", "victor", "vinegar", "violence", "visiting", "visual", "vocal", "vowel", "voyage", "vulture".
     
----Words starting with 'W':
+    """
+)
 
-"wakeful", "waltz", "watermelon", "weather", "welcome", "westerly", "whitener", "wholesome", "wisdom", "worrisome",
+st.write(
+    """
+ (W)"wakeful", "waltz", "watermelon", "weather", "welcome", "westerly", "whitener", "wholesome", "wisdom", "worrisome",
 "writing".
 
----Words starting with 'X':
 
-"xylem".
-
----Words starting with 'Y':
-
-"yank", "yardage", "yearly", "yoga", "youthful".
-
----Words starting with 'Z':
-
-"zap", "zest", "zookeeper".
-
-"""
-    
+    """
 )
+
+st.write(
+    """
+ (X)"xylem".
+    
+    """
+)
+
+st.write(
+    """
+ (Y)"yank", "yardage", "yearly", "yoga", "youthful".
+    
+    """
+)
+
+st.write(
+    """
+ (Z)"zap", "zest", "zookeeper".
+    
+    """
+)
+
