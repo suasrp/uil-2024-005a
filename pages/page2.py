39781f8e39a56da7255c82d852f958dac0a2c75b
suasrp <suasrp@gmail.com>
2024-12-19 15:34:08 -0600

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -224,25 +224,3 @@ else:
         
 ########################################################################################
 
-## Example alphabet test data
-#ALPHABET_TESTS = {
-    #'a': ["abbreviate", "abnormality"],
-    #'b': ["badminton", "balky"],
-    #'c': ["calculate", "calendar"],
-    #'d': ["damask", "dauntless"],
-    ## Add more letters and words...
-#}
-
-#st.write(
-#    """
-## 📝 5th-6th Grade - List of Words
-#    """
-#)
-
-#st.title("Select Your Test")
-#letter = st.selectbox("Select a letter", list(ALPHABET_TESTS.keys()))
-#st.write(f"Selected Test: {letter.upper()}")
-#st.write("Words in this test:")
-#st.write(ALPHABET_TESTS[letter])
-
-## You could add a function to track progress here
